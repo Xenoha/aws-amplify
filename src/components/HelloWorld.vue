e2e679be43c9a810d3a3e4e2df1ca52569a7cfc4
Adrian Green <agreen@reflectsystems.com>
2020-05-08 21:49:51 -0500

    One last header change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container class="mt-12">
-    <v-row class="mt-12 text-center">
+  <v-container>
+    <v-row class="text-center">
       <v-col cols="12">
         <v-img
           :src="require('../assets/logo.svg')"
